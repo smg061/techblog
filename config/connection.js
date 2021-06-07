d8ff81917d82a1518ac541f5a5ac8b4f21bf9bc9
Santos <max.go.95@gmail.com>
2021-06-07 11:08:03 -0500

    Modified the sequelize connection to make it work with remote jawsdb. Praying it works

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const env = process.env.NODE_ENV;
+const config = require(__dirname + '/../config/config.js')[env];
 
+let sequelize;
+if (process.env.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
+}
 
-const sequelize = new Sequelize(
-  process.env.JAWS_DB_NAME,
-  process.env.JAWS_DB_USER,
-  process.env.JAWS_DB_PASSWORD,
-  {
-    host: process.env.JAWS_DB_HOST,
-    dialect: 'mysql',
-    port: 3306
-  }
-
-);
-
-
-// else {
-//   const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306,p
-//     }
-//   );
-// }
+else {
+    sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 module.exports = sequelize;
